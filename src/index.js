c0f55282bebcfe905b14b4b964ca5f71c17513f6
JessiceM <jessica52mdz@gmail.com>
2022-02-22 18:55:45 -0600

    Fixed current date

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 // Displays current time in Dallas
-function timeDate(timestamp) {
-  let currentDate = document.querySelector("#dateTime");
-  let time = new Date(timestamp);
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  let day = days[time.getDay()];
-  let hours = time.getHours();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = time.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  currentDate.innerHTML = `${day} ${hour}:${minutes}`;
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
@@ -68,6 +75,8 @@ function getTemp(response) {
   let description = document.querySelector("#description");
   windDesc.innerHTML = Math.round(response.data.wind.speed);
   description.innerHTML = response.data.weather[0].description;
+  let currentDate = document.querySelector("#dateTime");
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 //2B. Get user coordinates
 function getCurrentPosition() {
